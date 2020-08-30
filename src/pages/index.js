22585a02daca60acde6900b5064298d2d530710b
Conner Matson <cnr.mtsn@gmail.com>
2020-08-30 12:40:13 -0500

    gitignore updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,15 @@ import styled from "styled-components";
 
 export default function Home() {
   const Card = styled.div`
+    background: #eff1f3;
+    background-size: cover;
     display: flex;
     justify-content: center;
     align-items: center;
     padding: 15px;
-    height: 400px;
-    width: 600px;
-    box-shadow: 4px 4px 15px 0px rgba(50, 50, 50, 0.75);
+    height: 150px;
+    width: 400px;
+    box-shadow: 4px 4px 20px 0px rgba(255, 255, 255, 0.75);
     border-radius: 10px;
   `;
 
@@ -25,6 +27,11 @@ export default function Home() {
           alignItems: "center",
           height: "100vh",
           width: "100vw",
+          background: "#43C6AC",
+          background:
+            "-webkit-linear-gradient(to top, #191654, #43C6AC)" /* Chrome 10-25, Safari 5.1-6 */,
+          background:
+            "linear-gradient(to top, #191654, #43C6AC)" /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */,
         }}
       >
         <Card>

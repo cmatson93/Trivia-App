444b81138182ecf24e494bb4bb72eb8ec9651015
Conner Matson <cnr.mtsn@gmail.com>
2020-09-01 20:40:42 -0500

    react app

---
+++ src/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <p>Trivia App</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/src/config.js
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    mint: "#0FC1AF",
+    mintHover: "#0DA99A",
+    smoke: "#C3D0D9",
+    slate: "#4B5D6B",
+    white: "#FFFFFF",
+  },
+};
+
+export default theme;

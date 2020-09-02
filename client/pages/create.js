e25cff8d43adf6ffd77ba7c194ac9163bb62d666
Conner Matson <cnr.mtsn@gmail.com>
2020-09-01 20:50:01 -0500

    fixed

---
+++ client/pages/create.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import Navbar from "../components/Navbar";
+
+export default function Create() {
+    return (
+        <div>Create</div>
+    )
+};
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}

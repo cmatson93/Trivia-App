28a90cdedbc4028b9e3d55cd1909830e3aee547e
Conner Matson <cnr.mtsn@gmail.com>
2020-09-01 19:29:58 -0500

    'added head to create page

--- src/pages/create.js
+++ src/pages/create.js
@@ -1,7 +1,14 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import Head from "next/head";
+import Navbar from "../components/Navbar";
 
 export default function Create() {
-    return (
-        <div>Create</div>
-    )
-};
+  return (
+    <div>
+      <Head>
+        <title>Create Trivia Game</title>
+      </Head>
+      <Navbar />
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,11 @@
 import Head from "next/head";
-import styled from "styled-components";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
-  const Card = styled.div`
-    background: #eff1f3;
-    background-size: cover;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 15px;
-    height: 150px;
-    width: 400px;
-    box-shadow: 4px 4px 20px 0px rgba(255, 255, 255, 0.75);
-    border-radius: 10px;
-  `;
-
   return (
     <div>
       <Head>
         <title>Trivia App</title>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet"/>
       </Head>
       <Navbar />
     </div>

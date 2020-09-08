a3645141fe0279eb4838d31d3aac1c4b1001577d
Christina  Ng <cmatson93@gmail.com>
2020-09-07 20:41:48 -0700

    query faunaDB to getAll Quizes

---
+++ src/utils/api.js
@@ -0,0 +1,19 @@
+const createQuiz = (data) => {
+    return fetch('/.netlify/functions/quizes-create', {
+        body: JSON.stringify(data),
+        method: 'POST'
+    }).then(response => {
+        return response.json()
+    })
+}
+
+const getAll = () => {
+    return fetch('/.netlify/functions/quizes-get-all').then((response) => {
+        return response.json() 
+    })
+}
+
+export default {
+    createQuiz: createQuiz,
+    getAll: getAll
+}
